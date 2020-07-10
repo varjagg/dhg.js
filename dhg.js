d4ce2e8c9ac6da1c8333f0191a3459b047f4d4bd
Eugene Zaikonnikov <eugene@funcall.org>
2020-07-11 00:56:19 +0200

    tweak radius for smoother projection rendering

--- dhg.js
+++ dhg.js
@@ -100,7 +100,7 @@ function tick() {
 function linkArc(d) {
   var dx = d.target.x - d.source.x,
       dy = d.target.y - d.source.y,
-      dr = Math.sqrt(dx * dx + dy * dy);
+      dr = Math.sqrt(dx * dx + dy * dy) * 4;
   return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
 }
 
